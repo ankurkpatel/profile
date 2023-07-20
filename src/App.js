85a4e6d265e8c4a72605f97b0d0cc94062b9a7ef
ankurkpatel <ankur_k_patel@outlook.com>
2023-07-20 19:44:27 -0400

    new resume

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import {Home} from './Home';
-
+import {Resume} from './Resume';
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />}/>
+        <Route path="/profile" element={<Resume />}/>
       </Routes>
     </BrowserRouter>
   );
---
+++ src/Resume.jsx
@@ -0,0 +1,297 @@
+import "./Resume.css";
+export const Resume = () => {
+  return (
+    <div className="c8 doc-content">
+      <p className="c25">
+        <span className="c15 c9 c12">Ankur Patel </span>
+        <span className="c7">
+          | ankurpatel7@outlook.com | +1-519-702-5907 | Toronto, ON |
+          Linkedin.com/in/ankurpatel7
+        </span>
+      </p>
+      <p className="c3">
+        <span className="c7"></span>
+      </p>
+      <p className="c27">
+        <span className="c1 c4">
+          Analytics Manager who facilitated decisions to increase revenue in
+          millions, saved 1000s of work hours through automation and impacted
+          the lives of 1000s of underprivileged students
+        </span>
+        <span className="c1 c17 c4">&nbsp;.</span>
+      </p>
+      <p className="c2">
+        <span className="c17 c22 c4 c28"></span>
+      </p>
+      <p className="c2">
+        <span className="c11 c10 c9"></span>
+      </p>
+      <p className="c14">
+        <span className="c7">Experience</span>
+      </p>
+      <p className="c14 c29">
+        <span className="c6 c9">Consultant(AI Data Products)</span>
+        <span className="c1 c16">&nbsp;</span>
+        <span className="c10 c9">| </span>
+        <span className="c10 c9 c12">
+          Sionic - A Davies Company Toronto, Canada
+        </span>
+        <span className="c10 c9">| </span>
+        <span className="c17 c10 c9 c12">Feb 2023 &ndash; Present</span>
+      </p>
+      <p className="c5">
+        <span className="c1">Regulatory Intelligence Q&amp;A Bot</span>
+        <span className="c10 c9 c16">
+          : Spearheaded efforts to build Q&amp;A chat bot using GPT 3.5 and
+          Langchain with only one analyst. Improved operational efficiency
+          building a web app, improved user satisfaction 70%.
+        </span>
+      </p>
+      <p className="c5">
+        <span className="c1">User Stories Alignment</span>
+        <span className="c10 c9 c16">
+          : Led cross-functional user stories and requirement gathering
+          sessions, achieving 100% stakeholder alignment, leading to a 20% reduction in
+          rework.
+        </span>
+      </p>
+      <p className="c5">
+        <span className="c1">Google Analytics Implementation</span>
+        <span className="c0">
+          : Implemented Google Analytics for data-driven decision-making, boosting user engagement by 25%, resulting in a 10% increase in tool
+          adoption and usage.
+        </span>
+      </p>
+      <p className="c19">
+        <span className="c0">
+    
+        </span>
+      </p>
+      <p className="c3">
+        <span className="c11 c10 c9"></span>
+      </p>
+      <p className="c14">
+        <span className="c6 c9">Analytics Manager</span>
+        <span className="c9 c10">&nbsp;| </span>
+        <span className="c7">EXL Gurgaon, India | Aug 2021 - Sep 2022</span>
+      </p>
+      <p className="c5">
+        <span className="c1">Marketing Spend Optimization</span>
+        <span className="c0">
+          : Boosted revenue by 9% by spearheading marketing analytics effort for
+          19 international markets. Estimate ROIs for 600+ offers to allocate
+          marketing spend efficiently.
+        </span>
+      </p>
+      <p className="c5">
+        <span className="c1">Incremental Customer Value</span>
+        <span className="c10 c9 c16">: </span>
+        <span className="c10 c9">Led a small team of 3</span>
+        <span className="c0">
+          &nbsp;data analysts to use ML techniques to predict customer
+          behaviors, such as spend, revolve balance, attrition, to estimate the
+          incremental lift in customer value.
+        </span>
+      </p>
+      <p className="c5">
+        <span className="c1">Process Improvement</span>
+        <span className="c0">
+          : Designed and implemented processes such as peer review, code update
+          workflow, maintenance using git-github. Designed a validation process
+          to check the accuracy of the reporting numbers.
+        </span>
+      </p>
+      <p className="c5">
+        <span className="c1">Project Management</span>
+        <span className="c0">
+          : Successfully planned and carried out yearly budget analytics for
+          marketing initiatives with the client, including defining technical
+          needs, applying predictive methodologies, establishing objectives, and
+          effectively communicating any issues to stakeholders.
+        </span>
+      </p>
+      <p className="c5">
+        <span className="c1">Dashboards</span>
+        <span className="c15 c9 c16">&nbsp;:</span>
+        <span className="c0">
+          &nbsp;Oversaw development of dashboards to display a 12-month forecast
+          of various customer behaviors. Improved accuracy around 11% by
+          updating predictions every month using newly available data.
+        </span>
+      </p>
+      <p className="c5">
+        <span className="c1">Data Pipeline</span>
+        <span className="c0">
+          : Decreased processing time by 20% for scripts handling 2 TBs of data
+          by converting them from Hive to PySpark. Designed &amp; built data
+          models for the team to facilitate analysis of customer behavior.
+        </span>
+      </p>
+      <p className="c3">
+        <span className="c11 c10 c9"></span>
+      </p>
+      <p className="c14">
+        <span className="c6 c9">BI Reporting</span>
+        <span className="c6">&nbsp;</span>
+        <span className="c6 c9">Specialist</span>
+        <span className="c15">&nbsp;</span>
+        <span className="c10">|</span>
+        <span className="c10 c23">&nbsp;</span>
+        <span className="c17 c18 c10">
+          Children&rsquo;s Literacy Initiative Philadelphia, PA | Jun 2017 - Jun
+          2020
+        </span>
+      </p>
+      <p className="c5">
+        <span className="c1">BI Dashboards - Tableau</span>
+        <span className="c0">
+          : Implemented a comprehensive BI solution, streamlining data
+          collection, aggregation, KPI development, and visualization, resulting
+          in a 75% decrease in report turnaround time.
+        </span>
+      </p>
+      <p className="c5">
+        <span className="c1">Legacy SSRS -&gt; Tableau Migration</span>
+        <span className="c0">
+          : Designed and implemented a roadmap for the company to migrate from
+          SSRS to Tableau.
+        </span>
+      </p>
+      <p className="c5">
+        <span className="c1">Data Warehouse</span>
+        <span className="c0">
+          : Built ETL process with Python + Informatica &nbsp;to load raw data
+          from different sources into a dimensional data model in SQL server to
+          reduce report execution time by 90%.
+        </span>
+      </p>
+      <p className="c5">
+        <span className="c1">CRM Revamp SalesForce</span>
+        <span className="c0">
+          : Steered Salesforce overhaul as Tech lead to resolve the misalignment
+          between the business model and data model to improve data quality and
+          reporting infrastructure. Saved 30 hours of manual data work every
+          month.
+        </span>
+      </p>
+      <p className="c5">
+        <span className="c1">Data Product</span>
+        <span className="c0">
+          : Managed the development of the first version of the Coaching Support
+          System, which helps nationwide teams of 120+ employees to collect and
+          access feedback data efficiently.
+        </span>
+      </p>
+      <p className="c5">
+        <span className="c1">Sentiment Analysis</span>
+        <span className="c0">
+          : Built an SVM Classifier for sentiment analysis of customer feedback
+          data to supplement customer satisfaction metrics.
+        </span>
+      </p>
+      <p className="c5 c21">
+        <span className="c0"></span>
+      </p>
+      <p className="c3">
+        <span className="c17 c10 c9 c4"></span>
+      </p>
+      <p className="c14">
+        <span className="c9 c15">Data Analytics</span>
+        <span className="c10 c9">&nbsp;Intern</span>
+        <span className="c10">&nbsp;| </span>
+        <span className="c10 c12 c20">
+          American Savings Bank Honolulu, HI | Jun 2016 &ndash; Aug 2016
+        </span>
+      </p>
+      <p className="c5">
+        <span className="c1">Streamline Reporting</span>
+        <span className="c0">
+          : Developed a script to collect, transform, and load data into a
+          centralized relational database, reducing manual effort by 98%.
+        </span>
+      </p>
+      <p className="c5">
+        <span className="c1">New ATM Locations</span>
+        <span className="c0">
+          : Reduced customer service costs by identifying 5 locations for ATM
+          deployment. Analyzed 1.5M customer transactions by utilizing advanced
+          data exploration in Tableau.
+        </span>
+      </p>
+      <p className="c5">
+        <span className="c1">Cross Departmental Analytics</span>
+        <span className="c0">
+          : Devised new metrics for HR and marketing departments for monitoring
+          Key Performance Indicators. Recommended solutions to solve a staffing
+          problem through Cross-functional data analysis.
+        </span>
+      </p>
+      <p className="c13 c21">
+        <span className="c10 c9 c4 c17"></span>
+      </p>
+      <p className="c24">
+        <span className="c15 c9">Programmer Analyst</span>
+        <span className="c10 c9">&nbsp;| </span>
+        <span className="c7">
+          Cognizant Technology Solution Pune, India | Dec 2012 &ndash; Sep 2014
+        </span>
+      </p>
+      <p className="c26">
+        <span className="c1">ETL Testing</span>
+        <span className="c10 c9 c12">: </span>
+        <span className="c0">
+          Planned and tested the functionality of ETL jobs for estatement ODS,
+          saved 27,000 INR by finding 27 bugs.
+        </span>
+      </p>
+      <p className="c5 c21">
+        <span className="c0"></span>
+      </p>
+      <p className="c13">
+        <span className="c7">Skills</span>
+      </p>
+      <p className="c5">
+        <span className="c10 c9 c12">Languages &amp; Libraries: </span>
+        <span className="c11 c10 c9">
+          Python, SQL, Hive, Pandas, Scikit-learn, Seaborn, PySpark, HTML, CSS,
+          JavaScript (React, Node)
+        </span>
+      </p>
+      <p className="c5">
+        <span className="c10 c9 c12">Technologies: </span>
+        <span className="c10 c9 c11">
+          Tableau, SSRS, Power BI, AWS, Informatica, GitHub, Git, JIRA,
+          Salesforce, LangChain, GPT
+        </span>
+      </p>
+      <p className="c13 c21">
+        <span className="c17 c15 c9 c16"></span>
+      </p>
+      <p className="c24">
+        <span className="c7">Education</span>
+      </p>
+      <p className="c13">
+        <span className="c17 c10 c18">
+          University of Texas at Dallas Richardson, TX | Aug. 2015 &ndash; May.
+          2017
+        </span>
+      </p>
+      <p className="c24">
+        <span className="c11 c10 c9">Master of Science in IT Management</span>
+      </p>
+      <p className="c13">
+        <span className="c7">
+          Nirma University Ahmedabad, India | Aug. 2008 &ndash; July. 2012
+        </span>
+      </p>
+      <p className="c13">
+        <span className="c11 c10 c9">
+          Bachelor of Technology in Electronics &amp; Communications
+        </span>
+      </p>
+      <p className="c13 c21">
+        <span className="c17 c10 c9 c12"></span>
+      </p>
+    </div>
+  );
+};
